33616e34f8e65dd0217d8d9ca49b79800f138054
Sakshi Sen <sakshisen31fg@gmail.com>
2026-06-02 11:21:11 +0530

    add hello.cpp with initial structure and commented code examples

---
+++ Basic/hello.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+ int main() 
+ {
+    // int a,b,c,d,e;
+    //  a=2;
+     // b=4;
+     // d=2;
+     // c=a+b;
+    // cout<<c<<endl;
+    // e=c / d;
+    //cout<<e;
+
+
+
+// int a ,b,c;
+// a=2;
+// b=3;
+// c=a+b;
+// cout<<c;
+
+
+
+// int a,b,sum;
+// cout<<"enter a number a";
+// cin>>a;
+// cout<<"enter a number b";
+// cin>>b;
+// sum=a+b;
+// cout<<"sum="<<sum;
+
+
+// int a,b,division;
+// cout<<"enter a number a";
+// cin>>a;
+// cout<<"enter a number b ";
+// cin>>b;
+// division=a/b;
+// cout<<"division="<<divisioi
+
+
+
+
+// int a,b,c;
+// cout<<"enter a vlue a";
+// cin>>a;
+// cout<<"enter a vlue b";
+// cin>>b;
+
+// c=a;
+
+// a=b;
+// b=c;
+// cout<<a<<endl<<b;
+
+// cout<<"the reminder will be "<<83%4;
+// string a;
+// cin>>a;
+// cout<<nextline()charAt(5);
+
+// 
+// int a,b,c;
+// a=b=c=10;
+// cout<<a<<endl<<b<<endl<<c;
+
+
+// int a;
+// a=(6&4==4);
+// cout<<a;
+
+// int a;
+// a=(5==10!=20);
+// cout<<a;
+
+
+
+// int a=2;
+// a+=2<<1;
+// cout<<a;
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
